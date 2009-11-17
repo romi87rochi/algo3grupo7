3e50d6d76db59e16371f425298eb789a9ac44a14
sadoly <sadoly@689ceaae-cfd4-11de-a711-9fae7a67fc1c>
2009-11-17 03:09:13 +0000

    Esqueleto de las clases Pacman y Personaje

---
+++ Documents/Personaje.java
@@ -0,0 +1,74 @@
+
+public class Personaje {
+	
+	private int posX;
+	private int posY;
+	
+	/**
+	 * Constructor
+	 */	
+	public Personaje() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * 
+	 */	
+	public void  comer() {
+		
+	}
+	
+	/**
+	 * 
+	 */
+	public void mover(){
+		
+	}
+	
+	/**
+	 * 
+	 */
+	public void cambiarEstado(){
+		
+	}
+	
+	/**
+	 * 
+	 */
+	public void revivir(){
+		
+	}
+	
+	/**
+	 * 
+	 */
+	public void setPosiciones(int x, int y){
+		this.posX = x;
+		this.posY = y;
+	}
+	
+	/**
+	 * 
+	 */
+	public int getX(){
+		return this.posX;
+	}
+	
+	/**
+	 * 
+	 */
+	public int getY(){
+		
+		return this.posY;
+	}
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
