91898826e165a2be0df60ff8d92fc5ef4f4e6f27
max.diego1988@gmail.com <max.diego1988@gmail.com@689ceaae-cfd4-11de-a711-9fae7a67fc1c>
2009-11-19 04:24:19 +0000

    Pruebas unitarias de la clase Blinky. Consultar con Nicolás.

---
+++ trunk/test/BlinkyTest.java
@@ -0,0 +1,42 @@
+import junit.framework.TestCase;
+
+public class BlinkyTest extends TestCase {
+
+	Blinky blinky;
+	int velocidad;
+	//int? estrategia;
+	int posX;
+	int posY;
+	
+	protected void setUp() throws Exception {
+		super.setUp();
+		velocidad = 1;
+		//estrategia = ?;
+		posX = 10;
+		posY = 10;
+		blinky = new Blinky(posX,posY,velocidad);
+	}
+	
+	public void testComerErroneo() {
+		try {
+			blinky.comer();
+			fail("Deberia lanzar excepcion por no poder comer a un fantasma");
+		}
+		catch(NoDeberiaSerComidoException e) {
+			assertTrue(true);
+		}
+	}
+	
+	public void testComerOk() {
+		try {
+			blinky.cambiarEstado();
+			blinky.comer();
+			assertEquals(posX,blinky.getX());
+			assertEquals(posY,blinky.getY());
+		}
+		catch(NoDeberiaSerComidoException e) {
+			fail("Falla al intentar comer a un fantasma que puede ser comido");
+		}
+	}
+
+}
