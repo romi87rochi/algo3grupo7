e5294fcc2bfe758c5b690ba6bca7ef45eed140c4
sadoly <sadoly@689ceaae-cfd4-11de-a711-9fae7a67fc1c>
2009-11-30 03:47:40 +0000

    Se volvió atras con los cambios a version 48 pero sacando el atributo juego de ItemComible

--- trunk/src/fiuba/grupo7algo3/modelo/ItemComible.java
+++ trunk/src/fiuba/grupo7algo3/modelo/ItemComible.java
@@ -1,33 +1,25 @@
 
 abstract class ItemComible {
-	private Juego juego;
-	private int puntajePredeterminado;
+	
+	private int     puntajePredeterminado;
 	private boolean esPowa;
 	
-	public ItemComible (Juego nuevoJuego, int nuevoPuntaje, boolean esPowa){
-		this.juego=nuevoJuego;
+	public ItemComible (int nuevoPuntaje, boolean esPowa){
+
 		this.puntajePredeterminado=nuevoPuntaje;
 		this.esPowa=esPowa;
-		
-		
+				
 	}
 	
 	public int getPuntaje(){
 		return this.puntajePredeterminado;
 	}
-
 	
-	public Juego getJuego(){
-		return juego;
-	}
-	public abstract void fueComido();
 	
 	protected boolean esDePoder(){
 		return esPowa;
 	}
+}
 	
 		
 	
-
-
-}
