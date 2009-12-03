4cd2db030d744705aaac27130dd94c3b1994a809
sadoly <sadoly@689ceaae-cfd4-11de-a711-9fae7a67fc1c>
2009-12-03 03:04:14 +0000

    Se agregó un "mapa harcodeado" dentro de la clase Tablero, por lo que se eliminó la clase Mapa. Se considera el siguiente supuesto: Todos los fantasmas tienen el mismo casiillero origen inicial.

--- trunk/src/fiuba/grupo7algo3/modelo/MatrizPosiciones.java
+++ trunk/src/fiuba/grupo7algo3/modelo/MatrizPosiciones.java
@@ -1,5 +1,4 @@
 
-
 import java.util.ArrayList;
 
 public class MatrizPosiciones {
@@ -8,70 +7,70 @@ public class MatrizPosiciones {
     private Posicion posiciones[][];
     
     
-	public MatrizPosiciones(int filas, int columnas) {
-		
-		this.filas    = filas;
-		this.columnas = columnas;
-		posiciones    = new Posicion[filas][columnas];
-		this.incializar();
-	}
+        public MatrizPosiciones(int filas, int columnas) {
+                
+                this.filas    = filas;
+                this.columnas = columnas;
+                posiciones    = new Posicion[filas][columnas];
+                this.incializar();
+        }
 
-	private void incializar() {
-		  int y;
-			  for(int x=0;x<filas;x++){
-				  y=0;
-				  for(y=0;y<columnas;y++){
-					  posiciones[x][y]= new Posicion(x,y,this);
-				  }
-			  }
-	}
+        private void incializar() {
+                  int y;
+                          for(int x=0;x<filas;x++){
+                                  y=0;
+                                  for(y=0;y<columnas;y++){
+                                          posiciones[x][y]= new Posicion(x,y,this);
+                                  }
+                          }
+        }
 
 
-	public Posicion obtenerPosicion(int posX, int posY) {
-		Posicion posicion = null;
-		try{
-			posicion = this.posiciones[posX][posY];
-		}
-		catch(Exception e){
-			//e.printStackTrace();
-			return null;
-		}
-		return posicion;
-	}
+        public Posicion obtenerPosicion(int posX, int posY) {
+                Posicion posicion = null;
+                try{
+                        posicion = this.posiciones[posX][posY];
+                }
+                catch(Exception e){
+                        //e.printStackTrace();
+                        return null;
+                }
+                return posicion;
+        }
 
-	public Posicion obtenerPosicion(Posicion posicion) {
-		Posicion pos = null;
-		try{
-			pos = this.posiciones[posicion.getPosX()][posicion.getPosY()];
-		}
-		catch(Exception e){
-			//e.printStackTrace();
-			return null;
-		}
-		return pos;
-	}
-	
-	public ArrayList<Posicion> getAdyacente(Posicion posicion){
-		Posicion arriba;
-		Posicion abajo;
-		Posicion izquierda;
-		Posicion derecha;
-		ArrayList<Posicion> posicionesAdyacenetes;
-		
-		posicionesAdyacenetes = new ArrayList<Posicion>(); 
-		arriba    = posicion.getArriba();
-		abajo     = posicion.getAbajo();
-		izquierda = posicion.getIzquierda();
-		derecha   = posicion.getDerecha();
-	
-		posicionesAdyacenetes.add(0,arriba);
-		posicionesAdyacenetes.add(1,abajo);
-		posicionesAdyacenetes.add(2,izquierda);
-		posicionesAdyacenetes.add(3,derecha);
-	
-		return posicionesAdyacenetes;
-	
-	}
+        public Posicion obtenerPosicion(Posicion posicion) {
+                Posicion pos = null;
+                try{
+                        pos = this.posiciones[posicion.getPosX()][posicion.getPosY()];
+                }
+                catch(Exception e){
+                        //e.printStackTrace();
+                        return null;
+                }
+                return pos;
+        }
+        
+        public ArrayList<Posicion> getAdyacente(Posicion posicion){
+                Posicion arriba;
+                Posicion abajo;
+                Posicion izquierda;
+                Posicion derecha;
+                ArrayList<Posicion> posicionesAdyacenetes;
+                
+                posicionesAdyacenetes = new ArrayList<Posicion>(); 
+                arriba    = posicion.getArriba();
+                abajo     = posicion.getAbajo();
+                izquierda = posicion.getIzquierda();
+                derecha   = posicion.getDerecha();
+        
+                posicionesAdyacenetes.add(0,arriba);
+                posicionesAdyacenetes.add(1,abajo);
+                posicionesAdyacenetes.add(2,izquierda);
+                posicionesAdyacenetes.add(3,derecha);
+        
+                return posicionesAdyacenetes;
+        
+        }
 
-	
+        
 }
