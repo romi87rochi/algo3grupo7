5569cd4381d46cb41c940cedc2bffd2a6e1ecabe
max.diego1988@gmail.com <max.diego1988@gmail.com@689ceaae-cfd4-11de-a711-9fae7a67fc1c>
2009-12-05 03:19:54 +0000

    Nuevos mapas. Método cargarTablero está definido ahora en clase Tablero.

--- trunk/src/fiuba/grupo7algo3/modelo/src/algo3/grupo7/algoman/modelo/MapaNivel2.java
+++ trunk/src/fiuba/grupo7algo3/modelo/src/algo3/grupo7/algoman/modelo/MapaNivel2.java
@@ -1,34 +1,34 @@
 package algo3.grupo7.algoman.modelo;
 
-public class MapaNivel1 extends Tablero {
-	
+public class MapaNivel2 extends Tablero {
+
 	private static final int PUNTAJEPUNTO = 20;
-	private static final int PUNTAJEPUNTOPODER = 60;
-	private static final int TIEMPODEPODER = 50;
-	private static final int PUNTAJEFRUTA = 100;
+	private static final int PUNTAJEPUNTOPODER = 65;
+	private static final int TIEMPODEPODER = 45;
+	private static final int PUNTAJEFRUTA = 200;
     private Casillero origenPacman; 
 	private Casillero origenFantasmas;
-  
-	public MapaNivel1(){
+	
+	public MapaNivel2(){
 		super(200,200);
-		origenPacman = getMatCasilleros()[100][40];
+		origenPacman = getMatCasilleros()[100][160];
 		origenFantasmas = getMatCasilleros()[100][100];
 	}
-
+	
 	protected void cargarContorno(){
 		construirCaminoDesdeHasta(10, 190, 10, 10,PUNTAJEPUNTO);
-		construirCaminoDesdeHasta(10, 10, 10, 190,PUNTAJEPUNTO);
-		construirCaminoDesdeHasta(190, 190, 10, 190,PUNTAJEPUNTO);
+		construirCaminoDesdeHasta(10, 10, 10, 40,PUNTAJEPUNTO);
+		construirCaminoDesdeHasta(10, 10, 160, 190,PUNTAJEPUNTO);
+		construirCaminoDesdeHasta(190, 190, 10, 40,PUNTAJEPUNTO);
+		construirCaminoDesdeHasta(190, 190, 160, 190,PUNTAJEPUNTO);
 		construirCaminoDesdeHasta(10, 190, 190, 190,PUNTAJEPUNTO);
 	}
 	
 	protected void cargarCaminosHorizontales(){
 		construirCaminoDesdeHasta(10, 190, 40, 40,PUNTAJEPUNTO);
-		construirCaminoDesdeHasta(10, 190, 70, 70,PUNTAJEPUNTO);
-		construirCaminoDesdeHasta(10, 190, 100, 100,PUNTAJEPUNTO);
-		construirCaminoDesdeHasta(10, 40, 130, 130,PUNTAJEPUNTO);
+		construirCaminoDesdeHasta(40, 160, 70, 70,PUNTAJEPUNTO);
+		construirCaminoDesdeHasta(40, 160, 100, 100,PUNTAJEPUNTO);
 		construirCaminoDesdeHasta(70, 130, 130, 130,PUNTAJEPUNTO);
-		construirCaminoDesdeHasta(160, 190, 130, 130,PUNTAJEPUNTO);
 		construirCaminoDesdeHasta(10, 190, 160, 160,PUNTAJEPUNTO);
 	}
 	
@@ -45,21 +45,21 @@ public class MapaNivel1 extends Tablero {
 	}
 	
 	protected void cargarPuntosDePoder(){
-		getMatCasilleros()[10][25].setItem(new
+		getMatCasilleros()[10][175].setItem(new
 				PuntoDePoder(PUNTAJEPUNTOPODER,TIEMPODEPODER));  
-        getMatCasilleros()[190][175].setItem(new
+        getMatCasilleros()[190][25].setItem(new
         		PuntoDePoder(PUNTAJEPUNTOPODER,TIEMPODEPODER));
 	}
 	
 	protected void cargarFruta(){
-		getMatCasilleros()[100][130].setItem(new Fruta(PUNTAJEFRUTA));
+		getMatCasilleros()[100][40].setItem(new Fruta(PUNTAJEFRUTA));
 	}
-	
-	public  Casillero getCasilleroOrigenFantasma(){
+		
+	public Casillero getCasilleroOrigenFantasma() {
 		return origenFantasmas;
 	}
-	
-	public Casillero getCasilleroOrigenPacman(){
-		return origenPacman;		
+
+	public Casillero getCasilleroOrigenPacman() {
+		return origenPacman;
 	}
 }
