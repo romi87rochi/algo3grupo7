a641f28c627cad3ca7933bdd3b3ae8a21c102ff0
romi87rochi <romi87rochi@689ceaae-cfd4-11de-a711-9fae7a67fc1c>
2009-12-06 19:45:12 +0000

    tp modificado con la idea de tener un metodo publico que modifica la posicion inicial de cada personaje, pero siempre trabajando con el mismo mapa en las pruebas

---
+++ branches/Tp con metodo para cambiar la posicion inicial de los personajes/Posicion.java
@@ -0,0 +1,65 @@
+package algo3.grupo7.algoman.modelo;
+public class Posicion {
+    private int posX;
+    private int posY;
+    private MatrizPosiciones matriz;
+    
+
+  
+	public Posicion(int posicionX, int posicionY, MatrizPosiciones matriz){
+            
+            this.matriz = matriz;
+            this.posX   = posicionX;
+            this.posY   = posicionY;
+            
+    }
+    public int getPosX(){
+            return posX;
+    }
+    
+    public int getPosY(){
+            return posY;
+    }
+    
+    public Posicion getArriba(){
+            Posicion arriba = null;
+            int posX = this.posX;
+            int posY = this.posY;
+            posX--;
+            
+            arriba = this.matriz.obtenerPosicion(posX,posY);
+            return arriba;
+    }
+    
+    public Posicion getAbajo(){
+            Posicion abajo = null;
+            int posX = this.posX;
+            int posY = this.posY;
+            posX++;
+            abajo = this.matriz.obtenerPosicion(posX,posY);
+            return abajo;
+    }       
+    
+    public Posicion getIzquierda(){
+            Posicion izquierda = null;
+            int posX = this.posX;
+            int posY = this.posY;
+            posY--;
+            izquierda = this.matriz.obtenerPosicion(posX,posY);
+            return izquierda;
+    }       
+
+    public Posicion getDerecha(){
+            Posicion derecha = null;
+            int posX = this.posX;
+            int posY = this.posY;
+            posY++;
+            derecha = this.matriz.obtenerPosicion(posX,posY);
+            return derecha;
+    }       
+    
+    public MatrizPosiciones getMatriz() {
+		return matriz;
+	}
+    
+}
---
+++ branches/Tp con metodo para cambiar la posicion inicial de los personajes/Punto.java
@@ -0,0 +1,8 @@
+package algo3.grupo7.algoman.modelo;
+public class Punto extends ItemComible{
+	
+	public Punto(int nuevoPuntaje){
+		super(nuevoPuntaje,false);
+	}
+	
+}
